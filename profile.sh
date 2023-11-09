2213efe2b7545e4f93417fe16da55f8c388648cb
jmeaster30 <jmeasterday@comcast.net>
2023-11-08 23:44:04 -0500

    Made improvement to the profiler script

--- profile.sh
+++ profile.sh
@@ -1,3 +1,4 @@
 export HYDRO_STD_ROOT=./standard_libraries/hydro
 cargo build --release
 valgrind --tool=callgrind target/release/ocean "$@"
+kcachegrind --desktopfile "$(find . -type f -exec ls -t1 {} + | head -1)"
